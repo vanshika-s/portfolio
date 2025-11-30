0a760c82059911f9390b67a826fd8fd24489f8b1
Vanshika S <63630298+vanshika-s@users.noreply.github.com>
2025-11-30 07:23:52 -0800

    theme choosing top right added

--- global.js
+++ global.js
@@ -62,3 +62,17 @@ for (let p of pages) {
     a.rel = "noopener noreferrer";
   }
 }
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+  <label class="color-scheme">
+    Theme:
+    <select id="color-scheme-select">
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+`
+);
