6e3907e165bd49b54673f75ad2415672594f8441
Vanshika S <63630298+vanshika-s@users.noreply.github.com>
2025-11-30 06:44:35 -0800

    adding finding link to current page

--- global.js
+++ global.js
@@ -3,3 +3,12 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$("nav a");
+console.log(navLinks);
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+console.log("Current link:", currentLink);
