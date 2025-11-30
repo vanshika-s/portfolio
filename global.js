75882a0fefefe8f44a968c4199432c0df825afdd
Vanshika S <63630298+vanshika-s@users.noreply.github.com>
2025-11-30 07:33:25 -0800

    Add working dark mode theme switcher

--- global.js
+++ global.js
@@ -76,3 +76,14 @@ document.body.insertAdjacentHTML(
   </label>
 `
 );
+
+const select = document.querySelector("#color-scheme-select");
+
+select.addEventListener("input", function (event) {
+  console.log("color scheme changed to", event.target.value);
+
+  document.documentElement.style.setProperty(
+    "color-scheme",
+    event.target.value
+  );
+});
